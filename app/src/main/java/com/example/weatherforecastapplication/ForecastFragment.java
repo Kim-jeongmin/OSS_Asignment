d0ba0be9d0d4105151ab31d096589af3ea61a062
Kim-jeongmin <55871387+Kim-jeongmin@users.noreply.github.com>
2020-05-30 11:29:56 +0900

    7th final

--- app/src/main/java/com/example/weatherforecastapplication/ForecastFragment.java
+++ app/src/main/java/com/example/weatherforecastapplication/ForecastFragment.java
@@ -86,13 +86,7 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        String[] data = {"Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"};
+        String[] data = {"날씨를 알고 싶은 도시를 선택해주세요."};
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
 
         adapter = new ArrayAdapter<String>(getActivity(), R.layout.activity_list_view_item, R.id.list_item_forecast_textview, weekForecast);
